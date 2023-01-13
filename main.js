1a821bce09e134f81e16fdfe3fe61be00d5e2356
alok <plantsfortrees@gmail.com>
2023-01-13 12:21:13 +0530

    Form Validation

--- main.js
+++ main.js
@@ -1,47 +1,57 @@
-const form = document.getElementById("form");
-const result = document.createElement("p");
+const fields = {};
 
-form.addEventListener("submit", function (e) {
-  const formData = new FormData(form);
-  e.preventDefault();
-  var object = {};
-  formData.forEach((value, key) => {
-    object[key] = value;
-  });
-  var json = JSON.stringify(object);
-  result.innerHTML = "Please wait...";
-
-  fetch("https://api.web3forms.com/submit", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: json,
-  })
-    .then(async (response) => {
-      let json = await response.json();
-      if (response.status == 200) {
-        result.innerHTML = json.message;
-        result.classList.remove("text-gray-500");
-        result.classList.add("text-green-500");
-        form.append(result);
-      } else {
-        console.log(response);
-        result.innerHTML = json.message;
-        result.classList.remove("text-gray-500");
-        result.classList.add("text-red-500");
-        form.append(result);
-      }
-    })
-    .catch((error) => {
-      console.log(error);
-      result.innerHTML = "Something went wrong!";
-    })
-    .then(function () {
-      form.reset();
-      setTimeout(() => {
-        result.style.display = "none";
-      }, 5000);
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  fields.fullName = document.getElementById("fullName");
+  fields.email = document.getElementById("email");
+  fields.message = document.getElementById("message");
 });
+
+function isNotEmpty(value) {
+  if (value === null || typeof value === "undefined") return false;
+  return value.length > 0;
+}
+
+function isEmail(email) {
+  let regex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+  return regex.test(String(email).toLowerCase());
+}
+
+function fieldValidation(field, validationFunction) {
+  if (field == null) return false;
+
+  let isFieldValid = validationFunction(field.value);
+  if (!isFieldValid) {
+    field.className = "placeholderRed";
+  } else {
+    field.className = "";
+  }
+
+  return isFieldValid;
+}
+
+function isValid() {
+  var valid = true;
+
+  valid &= fieldValidation(fields.fullName, isNotEmpty);
+  valid &= fieldValidation(fields.email, isEmail);
+  return valid;
+}
+
+class User {
+  constructor(fullName, email, message) {
+    this.fullName = fullName;
+    this.email = email;
+    this.message = message;
+  }
+}
+
+// Sending the contact form data with JavaScript
+function sendContact() {
+  if (isValid()) {
+    let user = new User(fullName.value, email.value, message.value);
+    alert(`${user.fullName} thanks for your message`);
+  } else {
+    alert(`Stay wherever you are`);
+  }
+}
